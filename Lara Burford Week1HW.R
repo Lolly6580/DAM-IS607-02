fed3c30ea3f206e04dfcb2f314e22c899009d6f7
Lolly6580 <Lolly6580@aol.com>
2014-08-30 11:34:32 -0400

    add week 1 homework

---
+++ Lara Burford Week1HW.R
@@ -0,0 +1,10 @@
+## 1. What version of R do you have installed?
+##    R version 3.1.1 (2014-07-10) -- "Sock it to Me"
+##    What version of R Studio do you have installed?
+##    R Studio Version 0.98.1028 - © 2009-2013 RStudio, Inc.
+
+## 2. What version of PostgreSQL do you have installed?
+##
+
+## 3. How many observations are contained in the data set sales?
+##    4011046 obs. of 5 variables
